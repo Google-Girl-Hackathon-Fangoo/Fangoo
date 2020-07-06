9e4b0c4801acb82e480e3da78a717435c33671da
FredySu <fredarthur4869@outlook.com>
2020-07-06 19:44:59 +0800

    Update SiderTwo.js

--- antd-demo/src/Components/SiderTwo.js
+++ antd-demo/src/Components/SiderTwo.js
@@ -20,6 +20,10 @@ const data1 = [
   'to-do 1',
   'to-do 2',
 ];
+const data2 = [
+  '今日?/总日程1',
+  '今日?/总日程2',
+];
 class ChooseForm extends Component{
   constructor(props){
     super(props);
@@ -90,6 +94,7 @@ class SiderTwo extends Component{
             style={{ height: '100%', borderRight: 0 }}
           >
             <SubMenu key="sub1" icon={<UserOutlined />} title="日程">
+            <MyList data={data2}/> 
             </SubMenu>
           </Menu>
         </Sider>
