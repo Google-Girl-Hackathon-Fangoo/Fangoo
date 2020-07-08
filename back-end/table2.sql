ccc424bd13baadf47cef0cb7c378af248dc86afb
moyiii-ai <19307130296@fudan.edu.cn>
2020-07-08 11:09:41 +0800

    Update table2.sql

--- back-end/table2.sql
+++ back-end/table2.sql
@@ -1,28 +1,28 @@
 create table flock (
     flock_id int not null auto_increment primary key,
     flock_name text,
-    admin_id int,
-    foreign key(admin_id) references user(user_id)
+    admin_name varchar(50),
+    foreign key(admin_name) references users(username),
 )
 
 create table flock_user (
-    user_id int,
+    users_name varchar(50),
     flock_id int,
     authority_type int,
-    foreign key(user_id) references user(user_id),
+    foreign key(users_name) references users(username),
     foreign key(flock_id) references flock(flock_id),
     primary key(flock_id, flock_user_id)
 )
 
 create table task (
     task_id int not null auto_increment primary key,
-    user_id int,
+    users_name varchar(50),
     task_name text,
     explain text,
     start_time datetime,
     end_time datetime,
     deadline datetime,
-    foreign key(user_id) references user(user_id)
+    foreign key(users_name) references users(username),
 )
 
 create table notice (
