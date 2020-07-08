ca9716f0a53ec75a00e16a28ed54636731414a80
moyiii-ai <19307130296@fudan.edu.cn>
2020-07-08 19:40:43 +0800

    create task
    api insert、delete、update for task

---
+++ back-end/routes/task.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+
+var connection = require('./database.js');
+
+router.use((req, res, next) => {
+  //  console.log('Success Time: ', Date.now())
+  next()
+})
+
+router.get('/', function(req, res) {
+  res.send('respond with a resource');
+});
+
+router.post('/insert', (req, res) => {
+  console.log(req.body)
+  data = req.body.data
+  console.log(data.username, data.title, data.deadline, data.description)
+  connection.query(
+    "insert into task(users_name, task_name, explain, deadline, finish) values(?, ?, ?, ?, 0)", [data.username, data.title, data.description, data.deadline],
+    function(error, results, fields) {
+      if (error) res.json({msg: error})
+    }
+  )
+  res.json({ msg: 'success'})
+})
+
+router.post('/update', (req, res) => {
+    console.log(req.body)
+    data = req.body.data
+    console.log(data.username, data.title)
+    getTime = new Date()
+    connection.query(
+      "update task set end_time = ?, finish = 1 where user_name = ? and title = ?", [getTime, data.username, data.title],
+      function(error, results, fields) {
+        if (error) res.json({msg: error})
+      }
+    )
+    res.json({ msg: 'success'})
+})
+
+router.post('/delete', (req, res) => {
+    console.log(req.body)
+    data = req.body.data
+    console.log(data.username, data.title)
+    connection.query(
+      "delete from task where user_name = ? and title = ?", [data.username, data.title],
+      function(error, results, fields) {
+        if (error) res.json({msg: error})
+      }
+    )
+    res.json({ msg: 'success'})
+})
+
+module.exports = router
--- back-end/table2.sql
+++ back-end/table2.sql
@@ -2,7 +2,7 @@ create table flock (
   flock_id int not null auto_increment primary key,
   flock_name text,
   admin_name varchar(50),
-  foreign key(admin_name) references users(username),
+  foreign key(admin_name) references users(username)
 )
 
 create table flock_user (
@@ -17,19 +17,13 @@ create table flock_user (
 create table task (
   task_id int not null auto_increment primary key,
   users_name varchar(50),
+  finish int default 0,
+  flock_id int,
   task_name text,
   explain text,
   start_time datetime,
   end_time datetime,
   deadline datetime,
   foreign key(users_name) references users(username),
-)
-
-create table notice (
-  notice_id int not null auto_increment primary key,
-  flock_id int,
-  notice_name text,
-  start_time datetime,
-  deadline datetime,
   foreign key(flock_id) references flock(flock_id)
 )
