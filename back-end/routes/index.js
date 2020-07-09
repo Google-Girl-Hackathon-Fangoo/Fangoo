e7a1b0ef8c8048129141babace5952afb255ea91
150042 <2482214580@qq.com>
2020-07-09 09:36:28 +0800

    register problem solved

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/hello', function(req, res, next) {
+	res.send('Hello World!');
+});
+
+
+router.get('/fanqie', function(req, res, next) {
+	res.send('番茄是大蛋');
+});
+
+
+
 module.exports = router;
