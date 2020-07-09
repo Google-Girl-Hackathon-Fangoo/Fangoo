a99ed7760d60a5023f7123ab25a0736921a9980e
150042 <2482214580@qq.com>
2020-07-09 11:33:39 +0800

    modify 'create flock module'

--- back-end/routes/flock.js
+++ back-end/routes/flock.js
@@ -1,38 +1,56 @@
 var express = require('express');
 var router = express.Router();
-var flockNum = 0
 
 var connection = require('./database.js');
 
 router.use((req, res, next) => {
-  //  console.log('Success Time: ', Date.now())
-  next()
-})
-
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+   next();
 });
 
-router.post('/addflock', (req, res) => {
-  console.log(req.body)
-  data = req.body.data
-  console.log(data.username, data.title)
-  flockNum = flockNum + 1
-  connection.query(
-    "insert into flock values(?, ?, ?)", [flockNum, data.username, data.title],
-    function(error, results, fields) {
-      if (error) res.json({msg: error})
-      else {
-        connection.query(
-          "insert into flockUser values(?, ?, 1)", [data.username, flockNum],
-          function(error, results, fields) {
-            if (error) res.json({msg: error})
-          }
-        )
-      }
-    }
-  )
-  res.json({ msg: 'success'})
-})
+// create flock
+router.post('/', (req, res) => {
+    console.log(req.body);
+    data = req.body.data;
+    console.log(data.username, data.title);
+    connection.query(
+        "insert into flock(adminName, flockName) values (?, ?)",
+        [data.username, data.title],
+        function (error, results, fields) {
+            if(error)
+                res.json({msg: error});
+            else
+            {
+                connection.query(
+                    "select flockId from flock where adminName= ? and flockName = ?",
+                    [data.username, data.title],
+                    function (error, results,fields) {
+                        if(error)
+                            res.json({msg: error});
+                        else
+                        {
+                            flockNum = results[0].flockId;
+                            connection.query(
+                                "insert into flockUser values(?, ?, 1)",
+                                [data.username, flockNum],
+                                function (error, results,fields) {
+                                    if(error)
+                                        res.json({msg: error});
+                                    else
+                                        res.json({msg: 'success'});
+                                }
+                            );
+                        }
+                    }
+                );
 
-module.exports = router
+            }
+        }
+    );
+});
+
+module.exports = router;
+
+
+
+
+
--- back-end/table2.sql
+++ back-end/table2.sql
@@ -1,9 +1,24 @@
 create table flock (
-  flockId int not null primary key,
+  flockId int auto_increment primary key,
   flockName text,
   adminName varchar(50),
   foreign key(adminName) references users(username)
-)
+);
+
+-- 限制(管理员,群组号)唯一
+delimiter $$
+create trigger tr_flock_before_insert
+before insert on flock
+for each row 
+begin 
+	DECLARE num int DEFAULT 0; 
+	select count(*) into num from flock where flockName=new.flockName and adminName=new.adminName;
+	if(num) then
+		insert into wrongtable values(0);
+	end if;
+end;
+$$
+delimiter ;
 
 create table flockUser (
   usersName varchar(50),
